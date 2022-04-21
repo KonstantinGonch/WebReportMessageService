1661dbeae901d1d7fb3d21b305f67f98b7cf6111
Goncharov Konstantin <goncharovkostya.1997@gmail.com>
2022-04-21 22:41:00 +0300

    Отправка по почте инцидента

--- WebReportMessageService/Controllers/IncidentReportController.cs
+++ WebReportMessageService/Controllers/IncidentReportController.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace WebReportMessageService.Controllers
 {
+    [EnableCors]
     [ApiController]
     [Route("api/incident")]
     public class IncidentReportController : ControllerBase
@@ -19,6 +23,31 @@ namespace WebReportMessageService.Controllers
                 report.FixationAt = DateTime.Now;
                 dbContext.IncidentReports.Add(report);
                 await dbContext.SaveChangesAsync();
+            }
+
+            try
+            {
+                var client = new SmtpClient("smtp.gmail.com", 587)
+                {
+                    Credentials = new NetworkCredential("testovtesttestovich13@gmail.com", "superFinashka"),
+                    EnableSsl = true
+                };
+                var message = $"Содержание: {report.Description}; Составлено: {report.FixationAt}; Вектор атаки: {report.AttackVector.ToString()}; Тип атаки: {report.TypeOfAttack.ToString()}; Необходимость содействия: {report.AssistanceNeeded.ToString()}";
+                client.Send("testovtesttestovich13@gmail.com", "goncharovkostya.1997@gmail.com", "Зарегистрировано новое обращение", message);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public IEnumerable<IncidentReport> Get()
+        {
+            using (var dbContext = new AppDataContext())
+            {
+                return dbContext.IncidentReports.ToList();
             }
         }
     }
