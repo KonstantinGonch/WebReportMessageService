a9ecb145b5b80056cdafdbe6a1f2a845d322eb19
Goncharov Konstantin <goncharovkostya.1997@gmail.com>
2022-04-25 22:26:31 +0300

    Интеграция с моделью

--- WebReportMessageService/Controllers/MessageController.cs
+++ WebReportMessageService/Controllers/MessageController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WebReportMessageService.Controllers
@@ -13,6 +17,7 @@ namespace WebReportMessageService.Controllers
     {
         private readonly ILogger<MessageController> _logger;
         private const int _pageSize = 8;
+        private HttpClient httpClient = new HttpClient();
 
         public MessageController(ILogger<MessageController> logger)
         {
@@ -21,11 +26,11 @@ namespace WebReportMessageService.Controllers
 
         [HttpGet]
         [Route("list")]
-        public  MessageListPageModel Get(int pageNumber)
+        public MessageListPageModel Get(int pageNumber)
         {
             using (var dbContext = new AppDataContext())
             {
-                var pageMessages = dbContext.Messages.OrderByDescending(m => m.MessageDate).Skip((pageNumber-1) * _pageSize).Take(_pageSize).ToList();
+                var pageMessages = dbContext.Messages.OrderByDescending(m => m.MessageDate).Skip((pageNumber - 1) * _pageSize).Take(_pageSize).ToList();
                 var totalPages = (int)Math.Ceiling(dbContext.Messages.Count() / (double)_pageSize);
                 return new MessageListPageModel { Messages = pageMessages, TotalPages = totalPages, PageNumber = pageNumber };
             }
@@ -38,6 +43,30 @@ namespace WebReportMessageService.Controllers
             using (var dbContext = new AppDataContext())
             {
                 message.MessageDate = DateTime.Now;
+
+                if (message.MessageType == MessageType.Unformal)
+                {
+                    try
+                    {
+                        var measurementContent = new StringContent(JsonConvert.SerializeObject(message), Encoding.UTF8, "application/json");
+                        var measurementResponse = await httpClient.PostAsync("http://localhost:5555/predict", measurementContent);
+                        var response = await measurementResponse.Content.ReadAsStringAsync();
+                        var o = JObject.Parse(response);
+                        var result = (string)o["result"];
+                        if (int.TryParse(result, out int pred))
+                        {
+                            if (pred == 1)
+                                message.MessageType = MessageType.ErrorReport;
+                            else
+                                message.MessageType = MessageType.Feedback;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+
                 dbContext.Add(message);
                 await dbContext.SaveChangesAsync();
             }
